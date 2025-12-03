a95af75cea80051f191219e7a196f987a6e54f4e
enzenoide <henzefernandes1974@gmail.com>
2025-12-03 18:16:59 +0000

    Implementei toda a parte de alocação dinâmica, falta a lista duplamente ligada

--- include/vector-lib/vector-array.hpp
+++ include/vector-lib/vector-array.hpp
@@ -3,64 +3,177 @@
 class vector_array {
 private:
     int *data;
-    unsigned int size_, capacity_;
-    void increase_capacity() {}
+    unsigned int size_, capacity_,increase_size_; // cada vector vai possuir um atributo size, capacity e increase_size
+    void increase_capacity() {
+        this -> capacity_ += this -> increase_size_; // para aumentar a capacidade, somar a capacity cm o increase_size
+        int *new_data = new int[this -> capacity_]; // alocar um novo array na memoria com o novo valor de capacity
+        for(int i = 0; i < this -> size_; ++i){
+            new_data[i] = data[i]; // copiar o array antigo no array novo
+        }
+        delete [] this -> data; // deletar o array antigo
+        this -> data = new_data; // o array atual vira o novo array
+    }
 
 public:
-    vector_array() {         // Construtor
+    vector_array() {
+        this -> size_ = 0;
+        this -> capacity_ = 100;
+        this -> increase_size_ = 100;
+        this -> data = new int[100];         
     }
     ~vector_array() { // Destrutor
+        delete [] this -> data;
     }
     unsigned int size() { // Retorna a quantidade de elementos armazenados
-        return 0;
+        return this -> size_;
     }
     unsigned int capacity() { // Retorna o espaço reservado para armazenar os elementos
-        return 0;
+        return this -> capacity_;
     }
     double percent_occupied() { // Retorna um valor entre 0.0 a 1.0 com o percentual da
-        return 0;               // memória usada.
+        return double(size()) / capacity();               // memória usada.
     }
     bool insert_at(unsigned int index, int value) { // Insere elemento no índice index
-        return false;
+        if(index > this -> size_){
+            return false;
+        }
+        if(this -> size_ == capacity_){
+            increase_capacity();
+        }
+        for(int i = this -> size_; i > index; --i){
+            data[i] = data[i-1];
+        }
+        data[index] = value;
+        this -> size_++;
+        return true;
     }
     bool remove_at(unsigned int index) { // Remove elemento do índice index
-        return false;
+        if(index >= this -> size_){
+            return false;
+        }
+        for(int i = index; i < this -> size_ - 1; ++i){
+            data[i] = data[i+1];
+        }
+        this -> size_--;
+        if(this -> size_ < this -> capacity_ / 4){ // se o tamanho da lista for menor que 1/4 da capacidade da lista, a lista reduz na metade
+            if(this -> capacity_ < 8){
+                return true;
+            }
+            this -> capacity_ /= 2;
+            int *new_data = new int[this -> capacity_];
+            for(int i = 0; i < this -> size_; ++i ){
+                new_data[i] = data[i];
+            }
+            delete[] data;
+            data = new_data;
+        }
+        return true;
     }
     bool is_empty() { // Retorna true se o vetor não contém elementos
-        return false;
+        if(this -> size_ == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    int get_at(unsigned int index) { // Retorna elemento no índice index,
-        return -1;                   // −1 se índice inválido
+    int get_at(unsigned int index) { // Retorna elemento no índice index                  // −1 se índice inválido
+        if(index >= this -> size_ ){
+            return -1;
+        }
+        return data[index];
     }
     void clear() { // Remove todos os elementos, deixando o vetor no estado inicial
+        //delete [] data;
+        //capacity_ = 100; // QUER Q VOLTE AO ESTADO INICIAL OU SÓ LIMPE?
+        //increase_size_ = 100;
+        //size_ = 0;
+        //data = new int[capacity_];
+        this -> size_ = 0;
     }
     void push_back(int value) { // Adiciona um elemento no ``final'' do vetor
+        if(this -> size_ == this -> capacity_){
+            increase_capacity();
+        }
+        data[this -> size_++] = value;
     }
     void push_front(int value) { // Adiciona um elemento no ``início'' do vetor
+        insert_at(0,value);
     }
     bool pop_back() {            // Remove um elemento do ``final'' do vetor
-        return false;
+        if(this -> size_ == 0){
+            return false;
+        }
+        this -> size_--;
+        return true;
     }
     bool pop_front() {           // Remove um elemento do ``início'' do vetor
-        return false;
+        if(this -> size_ == 0){
+            return false;
+        }
+        return remove_at(0);
     }
     int back() {                 // Retorna o elemento do ``final'' do vetor
-        return -1;
+        if(this -> size_ == 0){
+            return -1;
+        }
+        return data[this -> size_ -1];
     }
     int front() {                // Retorna o elemento do ``início'' do vetor
-        return -1;
+        if(this -> size_ == 0){
+            return -1;
+        }
+        return data[0];
     }
     bool remove(int value) {     // Remove value do vetor caso esteja presente
-        return false;            // Deve retornar true se value foi removido
+        int index = find(value);            // Deve retornar true se value foi removido
+        if(index == -1){
+            return false;
+        }
+        return remove_at(index);
     }
     int find(int value) {        // Retorna o índice de value, −1 caso value não esteja presente
+        for(int i = 0; i < this -> size_; ++i){
+            if(data[i] == value){
+                return i;
+            }
+        }
         return -1;
     }
     int count(int value) {       // Retorna quantas vezes value occorre no vetor
-        return 0;                // Retorna 0 se value não estiver presente
+        if(this -> size_ == 0){
+            return 0;
+        }
+        int count = 0;                // Retorna 0 se value não estiver presente
+        for(int i = 0; i < this -> size_; ++i){
+            if(data[i] == value){
+                count++;
+            }
+        }
+        return count;
     }
     int sum() {                  // Retorna a soma dos elementos do vetor
-        return 0;                // Retorna 0 se o vetor estiver vazio
+        int sum = 0;                // Retorna 0 se o vetor estiver vazio
+        if(this -> size_ == 0){
+            return 0;
+        }
+        for(int i = 0; i < this -> size_; ++i){
+            sum += data[i];
+        }
+        return sum;
+    }
+    bool remove_all(int value){
+        bool removed = false; // seta uma variavel bool para false para poder fazer a verificação
+        while(remove(value)){ // enquanto tiver valor, (remove = true)  a variavel removed vai ser true
+            removed = true; //
+        }
+        return removed; // retornar a variavel
+    }
+    bool remove_range(int a, int b){
+        for(int i = a; i < b;++i){
+            remove(data[i]);
+        }
+        return true;
     }
 };
 #endif // __VECTOR_LIST_IFRN__
--- include/vector-lib/vector-linked.hpp
+++ include/vector-lib/vector-linked.hpp
@@ -11,11 +11,31 @@ private:
 
 public:
     vector_linked() {
+        this -> head = 0;
+        this -> tail = 0; 
     }
     ~vector_linked() {
+        int_node *current,*to_remove;
+        current = this -> head;
+        while(current != nullptr){
+            to_remove = current;
+            current = current -> next;
+            delete current;
+        }
     }
     unsigned int size() { // Retorna a quantidade de elementos armazenados
-        return 0;
+        int i = 0;
+        if(is_empty()){
+            return 0;
+        }
+        if(head == tail){
+            return 1;
+        }
+        int_node *current = head;
+        while(current != nullptr){
+            i++;
+        }
+        return i;
     }
     unsigned int capacity() { // Retorna o espaço
         return 0;
@@ -24,46 +44,164 @@ public:
         return 0;               // com o percentual da memória usada.
     }
     bool insert_at(unsigned int index, int value) { // Insere elemento no índice index
-        return false;
+        int_node* current = head;
+        int_node *new_node = new int_node;
+        if(index < 0 || index > size_){
+            return false;
+        }
+        for(int i = 0; i < index; ++i){
+            current = current -> next;
+        }
+        if(this -> head == this -> tail){
+
+        }
+        new_node -> prev = current -> prev; // o prev do new_node aponta para o prev do atual
+        new_node -> next = current -> next; // o next do new_node aponta para o next do current
+        current -> prev -> next = new_node; // o next do prev do current aponta para o new_node
+        current -> prev = new_node; // o prev do current aponta para o new_node
+
+        size_++;
+        return true;
     }
     bool remove_at(unsigned int index) { // Remove elemento do índice index
         return false;
     }
     bool is_empty() { // Retorna true se o vetor não contém elementos
-        return false;
+        if(this -> head == 0){
+            return true;
+        }
+        else{
+            return false;
+        };
     }
     int get_at(unsigned int index) { // Retorna elemento no índice index,
         return -1;                   // −1 se índice inválido
     }
     void clear() { // Remove todos os elementos, deixando o vetor no estado inicial
+        int_node *current,*to_remove;
+        current = this -> head;
+        while(current != nullptr){
+            to_remove = current;
+            current = current -> next;
+            delete current;
+        }
     }
     void push_back(int value) { // Adiciona um elemento no ``final'' do vetor
+        int_node *new_node = new int_node;
+        new_node -> value = value; // o valor do new_node aponta para value
+        new_node -> next = nullptr; // o next de new_node aponta para NULL
+        new_node -> prev = this -> tail; // o anterior do new_node aponta para o atual tail
+
+        if(is_empty()){ // se a lista n tiver head o head aponta para o new_node
+            this -> head = new_node;
+        }
+        else{ // se não
+            this -> tail -> next = new_node; // o next do tail aponta para o new_node
+        }
+        this -> tail = new_node; // o tail agora aponta para o new_node
     }
     void push_front(int value) { // Adiciona um elemento no ``início'' do vetor
+        int_node *new_node = new int_node;
+        new_node -> value = value;
+        new_node -> next = this -> head;
+        new_node -> prev = nullptr;
+        if(this -> tail == nullptr){
+            this -> tail = new_node;
+        }
+        else{
+            this -> head -> prev = new_node;
+        }
+        this -> head = new_node;
     }
     bool pop_back() { // Remove um elemento do ``final'' do vetor
-        return false;
+        if(is_empty()){
+            return;
+        }
+        if(this -> head == this -> tail){
+            delete this -> head;
+            this -> head = nullptr;
+            this -> tail = nullptr;
+        }
+        this -> tail = this -> tail -> prev;
+        delete this -> tail;
+        this -> tail -> next = nullptr;
     }
     bool pop_front() { // Remove um elemento do ``início'' do vetor
-        return false;
+        if(is_empty()){
+            return;
+        }
+        if(this -> head == this -> tail){
+            delete this -> head;
+            this -> head = nullptr;
+            this -> tail = nullptr;
+        }
+        this -> head = this -> head -> next;
+        delete this -> head -> prev;
+        this -> head -> prev = nullptr;
     }
     int back() { // Retorna o elemento do ``final'' do vetor
-        return -1;
+        if(is_empty()){
+            return -1;
+        }
+        return this -> tail -> value;
     }
     int front() { // Retorna o elemento do ``início'' do vetor
-        return -1;
+        if(is_empty()){
+            return -1;
+        }
+        return this -> head -> value;
     }
     bool remove(int value) { // Remove value do vetor caso esteja presente
-        return false;
+        
     }
     int find(int value) { // Retorna o índice de value, −1 caso value não esteja presente
+        int i = 0;
+        int_node *current = head;
+        if(is_empty()){
+            return -1;
+        }
+        if(this -> head == this -> tail){
+            return 0;
+        }
+        while(current != nullptr){
+            if(current -> value == value){
+                return i;
+            }
+            else{
+                ++i;
+            }
+        }
         return -1;
     }
     int count(int value) { // Retorna quantas vezes value occorre no vetor
-        return 0;
+        int count = 0;
+        int_node *current = head;
+        if(is_empty()){
+            return 0;
+        }
+        if(this -> head == this -> tail){
+            return 1;
+        }
+        while(current != nullptr){
+            if(current -> value == value){
+                count ++;
+            }
+        }
+        return count;
     }
     int sum() { // Retorna a soma dos elementos do vetor
-        return 0;
+        int ans = 0;
+        int_node *current = head;
+        if(is_empty()){
+            return 0;
+        }
+        if(this -> head == this -> tail){
+            return 0;
+        }
+        while(current != nullptr){
+            ans += current -> value;
+        }
+        return ans;
     }
 };
 #endif // __VECTOR_LINKED_IFRN__
