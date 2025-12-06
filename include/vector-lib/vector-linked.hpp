aae561fb0f877e68086260efa1d7f61fc5a1d375
enzenoide <henzefernandes1974@gmail.com>
2025-12-06 03:56:27 +0000

    consertei o  remove_At

--- include/vector-lib/vector-linked.hpp
+++ include/vector-lib/vector-linked.hpp
@@ -15,13 +15,15 @@ public:
         this -> tail = 0; 
     }
     ~vector_linked() {
-        int_node *current,*to_remove;
-        current = this -> head;
+        int_node *current = this -> head;
+        int_node *to_remove;
         while(current != nullptr){
             to_remove = current;
             current = current -> next;
-            delete current;
+            delete to_remove;
         }
+        this -> head = nullptr;
+        this -> size_ = 0;
     }
     unsigned int size() { // Retorna a quantidade de elementos armazenados
         unsigned int i = 0;
@@ -82,27 +84,58 @@ public:
         return true;
     }
     bool remove_at(unsigned int index) { // Remove elemento do índice index
-        return false;
+        int_node *to_delete = head;
+        if(index > size_ || size_ == 0){
+            return false;
+        }
+        for(unsigned i = 0; i < index; ++i){
+            to_delete = to_delete -> next;
+        }
+        if(to_delete == head){
+            head = to_delete -> next;
+        }
+        if(to_delete == tail){
+            tail = to_delete -> prev;
+        }
+        if(to_delete -> next != nullptr){
+            to_delete -> next -> prev = to_delete -> prev;
+        }
+        if(to_delete -> prev != nullptr){
+            to_delete -> prev -> next = to_delete -> next;
+        }
+
+        delete to_delete;
+        size_--;
+        return true;
     }
     bool is_empty() { // Retorna true se o vetor não contém elementos
-        if(this -> head == 0){
+        if(this -> head == nullptr){
             return true;
         }
         else{
             return false;
         };
     }
-    int get_at(unsigned int index) { // Retorna elemento no índice index,
-        return -1;                   // −1 se índice inválido
+    int get_at(unsigned int index) { // Retorna elemento no índice index,                   // −1 se índice inválido
+        if(index >= size_ || size_ == 0){
+            return -1;
+        }
+        int_node *current = head;
+        for(unsigned i = 0; i < index; ++i){
+            current = current -> next;
+        }
+        return current -> value;
     }
     void clear() { // Remove todos os elementos, deixando o vetor no estado inicial
-        int_node *current,*to_remove;
-        current = this -> head;
+        int_node *current = this -> head;
+        int_node *to_remove;
         while(current != nullptr){
             to_remove = current;
             current = current -> next;
-            delete current;
+            delete to_remove;
         }
+        this -> head = nullptr;
+        this -> size_ = 0;
     }
     void push_back(int value) { // Adiciona um elemento no ``final'' do vetor
         int_node *new_node = new int_node;
@@ -132,6 +165,7 @@ public:
         this -> head = new_node;
     }
     bool pop_back() { // Remove um elemento do ``final'' do vetor
+        int_node *to_delete = tail;
         if(is_empty()){
             return false;
         }
@@ -140,9 +174,15 @@ public:
             this -> head = nullptr;
             this -> tail = nullptr;
         }
-        this -> tail = this -> tail -> prev;
-        delete this -> tail;
-        this -> tail -> next = nullptr;
+        else{
+        this -> tail = to_delete -> prev;
+        this-> tail -> next = nullptr;
+        delete to_delete;
+
+        }
+        size_--;
+        return true;
+        
     }
     bool pop_front() { // Remove um elemento do ``início'' do vetor
         if(is_empty()){
@@ -171,7 +211,7 @@ public:
         return this -> head -> value;
     }
     bool remove(int value) { // Remove value do vetor caso esteja presente
-        
+        return false;
     }
     int find(int value) { // Retorna o índice de value, −1 caso value não esteja presente
         int i = 0;
